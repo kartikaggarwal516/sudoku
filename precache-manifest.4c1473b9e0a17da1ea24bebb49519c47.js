c8f3e479fd28d83e9a410f0716f4759001f54b0d
Kartik <aggarwalkartik96@gmail.com>
2020-05-31 19:24:12 +0530

    Updates

---
+++ precache-manifest.4c1473b9e0a17da1ea24bebb49519c47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc7b64102142433fafcef9e529567d7",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "a2cfa68c5436aa6fc4df",
+    "url": "/sudoku/static/css/main.598a5335.chunk.css"
+  },
+  {
+    "revision": "40fd815cc7a5f714f9d8",
+    "url": "/sudoku/static/js/2.cf9efd28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku/static/js/2.cf9efd28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2cfa68c5436aa6fc4df",
+    "url": "/sudoku/static/js/main.5e35ac99.chunk.js"
+  },
+  {
+    "revision": "5e467a82d5680057536a",
+    "url": "/sudoku/static/js/runtime-main.e006c59b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sudoku/static/media/logo.5d5d9eef.svg"
+  }
+]);
